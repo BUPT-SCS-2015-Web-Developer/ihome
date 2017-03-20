23412a68e00684616b199ae2299b365f15ab47e9
liuzirui1122 <liuzirui1122@163.com>
2017-03-21 03:08:09 +0800

    Index.php基本完成
    1、因为首页默认搜索recommend问题， 而php中没处理，所以目前首页只会显示加载中
    2、自动补全部分有待补充
    3、php中建议添加取消warning,一定要加上header json

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,15 +4,32 @@ qSettings.start = 0;
 qSettings.contentNum = 80;
 qSettings.prevSettings = {};
 qSettings.full = false;
+qSettings.type = "";
 var Ques = function(quesID,qs) {
     var errormsg = {};
     errormsg['error'] = "加载问题详情时遇到数据库错误.";
     errormsg['login first']="请先登录!";
     errormsg['failed']="该问题可能已经被删除!";
+    var typeName = {};
+    typeName[1]="生活服务";
+    typeName[2]="问题反馈";
     var timeFormat = function(time) {
         var i = time.indexOf(".");
         if (i>0) return new timeago().format(time.substring(0,i)); 
         else return new timeago().format(time); 
+    }
+    var miliTimeFormat = function(time) {
+        var i = time.indexOf(".");
+        if (i>0) return Date.parse(time.substring(0,i)); 
+        else return Date.parse(time); 
+    }
+    var typeFormat = function(type) {
+        try {
+            return typeName[type];
+        } catch(e) {
+            Materialize.toast("数据库错误!",3000);
+        }
+        return "";
     }
     var alertStatus = function(type) {
         if (type=="success") return true;
@@ -34,7 +51,10 @@ var Ques = function(quesID,qs) {
     if (qs) {
         this.loaded = true;
         this.id = qs.id;
+        this.type = typeFormat(qs.type);
+        this.typeid = qs.type;
         this.time = timeFormat(qs.create_time);
+        this.miliTime = miliTimeFormat(qs.create_time);
         this.subject = qs.subject;
         this.content = contentComp(qs.content,qSettings.contentNum);
         this.hot = qs.hot;
@@ -47,6 +67,9 @@ var Ques = function(quesID,qs) {
                 ques.loaded = true;
                 ques.id = quesID;
                 ques.time = timeFormat(qdata['create_time']);
+                ques.miliTime = miliTimeFormat(qdata['create_time']);
+                ques.type = typeFormat(qdata['type']);
+                ques.typeid = qdata['type'];
                 ques.subject = qdata['subject'];
                 ques.content = contentComp(qdata['content'],qSettings.contentNum);
                 ques.hot = qdata['hot'];
@@ -65,13 +88,16 @@ var App = function() {
     errormsg['login first']="请先登录!";
     var currQ = [];
     var sQ = $("<div class='ques'></div>");
-    sQ.html("<div class='timeBox'>8天前</div><div class='mainQuesBox' data-id='110'><h5>标题</h5><p>正文</p></div><div class='otherBox'><p><i class='material-icons dp48'>star</i><span id='starNum'>9999</span>&nbsp;&nbsp;<i class='material-icons dp48'>comment</i><span id='commentNum'>9</span></p></div>");
+    sQ.html("<div class='timeBox'>8天前</div><div class='mainQuesBox' data-id='110'><h5>标题</h5><p>正文</p></div><div class='otherBox'><p><i class='material-icons dp48'>star</i><span class='starNum'>9999</span>&nbsp;&nbsp;&nbsp;<i class='material-icons dp48'>comment</i><span class='commentNum'>9</span>&nbsp;&nbsp;<span class='typeBox'><span></p></div>");
     var toggleStatus = function(str) {
         $("#moreText").text(str);
     }
-    
-    var removeAll = function() {
+    var removeAllQues = function() {
         $(".ques").remove();
+    }
+    var removeAllData = function() {
+        $(".ques").remove();
+        currQ = [];
     }
     var alertStatus = function(type) {
         if (type=="success") return true;
@@ -83,50 +109,64 @@ var App = function() {
         return false;
     }
     this.newQues = function(ques) {
+        //console.log(ques.miliTime);
         var sQnew = sQ.clone();
         sQnew.find(".timeBox").text(ques.time);
         sQnew.find(".mainQuesBox").data("id",ques.id);
+        sQnew.find(".mainQuesBox").attr("data-id",ques.id);
         sQnew.find("h5").text(ques.subject);
         sQnew.find(".mainQuesBox").children("p").text(ques.content);
-        sQnew.find("#starNum").text(ques.hot);
-        sQnew.find("#commentNum").text(ques.commentNum);
+        sQnew.find(".starNum").text(ques.hot);
+        sQnew.find(".typeBox").text(ques.type);
+        sQnew.find(".commentNum").text(ques.commentNum);
         $("#mainQuesBox").append(sQnew);
+        sQnew.find("h5").click(function(e){     
+            var id= $(this).parent().data("id");
+            console.log("clickEvent:",id);
+            window.location.href = "question.php?id="+id;
+            e.preventDefault();
+            e.stopPropagation();
+            return;
+        });
     }
     var search = function(sSettings) {
         toggleStatus("加载中...");
         qSettings.full = false;
+        //console.log(sSettings);
         $.post("API/getlist.php",{
-            text:sSettings.text0,
+            search:sSettings.text0,
             type:sSettings.type0,
             sortby:sSettings.sortby0,
             start:sSettings.start0
         },function(data){
             if(alertStatus(data['status'])) {
+                //console.log("search:",data);
                 var qldata = data['data'];
                 var l = qldata.length;
+                toggleStatus("下拉加载更多...");
                 if (l<10) {
-                    if (l==0) Materialize.toast("没有结果!",3000);
+                    if (l==0) Materialize.toast("没有查询到数据!",3000);
                     toggleStatus("没有更多了.");
                     qSettings.full = true;
                 }
-                for (var i=0;i<l;i++) {
-                    var newQ = new Ques(null,qldata[i]);
-                    app.newQues(newQ);
-                    currQ.push(newQ);
-                }
+                for (var i=0;i<l;i++) 
+                    if (qSettings.type=="" || qldata[i]['type'] == qSettings.type) {
+                        var newQ = new Ques(null,qldata[i]);
+                        app.newQues(newQ);
+                        currQ.push(newQ);
+                    }         
                 qSettings.start = 10;
                 qSettings.prevSettings = sSettings;
                 qSettings.prevSettings.start0 = 10;
-                toggleStatus("下拉加载更多...");
             } else {
                 toggleStatus("加载失败，请重新搜索.");
             }
         });
     }
-    var more = function(me) {
+    var more = function() {
         toggleStatus("加载中...");
         $.post("API/getlist.php",{
-            text:qSettings.prevSettings.text0,
+            search:qSettings.prevSettings.text0,
             type:qSettings.prevSettings.type0,
             sortby:qSettings.prevSettings.sortby0,
             start:qSettings.prevSettings.start0
@@ -134,32 +174,66 @@ var App = function() {
             if(alertStatus(data['status'])) {
                 var qldata = data['data'];
                 var l = qldata.length;
+                //console.log(data);
+                toggleStatus("下拉加载更多...");
+                qSettings.start += 10;
+                qSettings.prevSettings.start0 +=10; 
                 if (l<10) {
-                    if (l==0) Materialize.toast("没有结果!",3000);
+                    if (l==0) Materialize.toast("没有查询到数据!",3000);
                     toggleStatus("没有更多了.");
                     qSettings.full = true;
+                    qSettings.start = 0;
                 }
-                for (var i=0;i<l;i++) {
-                    var newQ = new Ques(null,qldata[i]);
-                    app.newQues(newQ);
-                    currQ.push(newQ);
-                }
-                qSettings.start += 10;
-                qSettings.prevSettings.start0 +=10;
-                toggleStatus("下拉加载更多...");
+                for (var i=0;i<l;i++) 
+                    if (qSettings.type=="" || qldata[i]['type'] == qSettings.type) {
+                        var newQ = new Ques(null,qldata[i]);
+                        app.newQues(newQ);
+                        currQ.push(newQ);
+                    }             
             } else {
                 toggleStatus("加载失败，请重新搜索.");
             }
-            me.resetload();
         });
     }
-    
+    var sortFunc = {
+        hot:function(a,b) {
+            return b.hot-a.hot;
+        },
+        time:function(a,b) {
+            return b.miliTime-a.miliTime;
+        }
+    }
+    var sort = function(type,sort) {
+        if (sort) {
+            //console.log(sort);
+            if (sort=="hot" || sort=="time") currQ.sort(sortFunc[sort]);
+            else if (sort == "recommended") {
+                removeAllData();
+                search({
+                    text0:"",
+                    type0:"",
+                    sortby0:"recommended",
+                    start0:0
+                });
+                return;
+            }
+            else {
+                Materialize.toast("排序类型不存在!",3000);
+                return;
+            }
+        }
+        removeAllQues();
+        
+        for (var i=0;i<currQ.length;i++) 
+            if (qSettings.type=="" || currQ[i].typeid == qSettings.type) {
+                //console.log(i);
+                app.newQues(currQ[i]);
+            }
+                
+            
+    }
     
     var eventInitButtons = function() {
-        $(".ques h5").click(function(){
-            var id= $(this).parent().data("id");
-            window.location.href = "question.php?id="+id;
-        });
         $("#searchImage").click(function(){
             var sSettings = {};
             sSettings.type0 = $('input[name="searchType"]:checked').val();
@@ -174,14 +248,27 @@ var App = function() {
                 Materialize.toast("搜索类型不符!",3000);
                 return;
             }
-            $(".ques").remove();
+            removeAllData();
             search(sSettings);
         });
-        $(".sort").click(function() {
-           if (!$(this).hasClass("active")) {
-               $(".sort.active").removeClass("active");
-               $(this).addClass("active");
-           } 
+        $(".sort").click(function(e) {
+            if (!$(this).hasClass("active")) {
+                $(".sort.active").removeClass("active");
+                $(this).addClass("active");
+            }
+            sort(qSettings.type,$(this).data("val"));
+            e.preventDefault();
+            return;
+        });
+        $(".type").click(function(e) {
+            if (!$(this).hasClass("active")) {
+                $(".type.active").removeClass("active");
+                $(this).addClass("active");
+            }
+            qSettings.type = $(this).data("val");
+            sort(qSettings.type);
+            e.preventDefault();
+            return;
         });
         
     }
@@ -197,11 +284,9 @@ var App = function() {
             limit: 20,
         });
         
-        //下拉加载
-        $("#mainQuesBox").dropload({
-            scrollArea : window,
-            loadDownFn : function(me){
-                if (!qSettings.full) more(me);
+        $(window).scroll(function() {
+            if (!qSettings.full && $(document).scrollTop() >= $(document).height() - $(window).height()) {
+                more();
             }
         });
     }
@@ -209,7 +294,6 @@ var App = function() {
     var eventInit = function() {
         //Init buttons event listeners
         eventInitButtons();
-        
         //Init forms
         eventInitForms();
     }
@@ -219,8 +303,8 @@ var App = function() {
         search({
             text0:"",
             type0:"",
-            //sortby0:"recommended",
-            sortby0:"hot",
+            sortby0:"recommended",
+            //sortby0:"hot",
             start0:0
         });
     }
--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <link href="assets/css/materialize.min.css" type="text/css" rel="stylesheet" />
     <link href="assets/css/index.css" type="text/css" rel="stylesheet" />
     <link href="assets/css/materialize_font.css" rel="stylesheet" />
+    <!--<link href="assets/css/dropload.css" rel="stylesheet" />-->
     <style>
     </style>
 </head>
@@ -46,15 +47,15 @@
         <div class="row a1">
         <div class="col s11">
         <div class="input-field">
-            <input placeholder="某热点问题..." id="searchBox" type="text" data-length="10">
+            <input placeholder="关键词尽量简洁，如宿舍，选课，空调等。 " id="searchBox" type="text" data-length="10">
             <label for="icon_prefix2">输入你想查询的内容</label>
         </div>
         <p>
-            <input class="with-gap" name="searchType" type="radio" id="test1" val="" checked/>
+            <input class="with-gap" name="searchType" type="radio" id="test1" value="" checked/>
             <label for="test1">全部&nbsp;</label>
-            <input class="with-gap" name="searchType" type="radio" id="test2" val="1" />
+            <input class="with-gap" name="searchType" type="radio" id="test2" value="1" />
             <label for="test2">生活服务&nbsp;</label>
-            <input class="with-gap" name="searchType" type="radio" id="test3" val="2"  />
+            <input class="with-gap" name="searchType" type="radio" id="test3" value="2"  />
             <label for="test3">问题反馈&nbsp;</label>
         </p>
         </div>
@@ -68,11 +69,11 @@
     <div class="quesBox container box white">
         <div id="quesHeader">
             <div id="typeBox">
-                <p><a class="type active" id="typeAll" href="#">全部问题</a><a class="type" id="typeAll" href="#">生活服务</a><a class="type" id="typeAll" href="#">问题反馈</a></p>
+                <p><a class="type active" id="typeAll" href="#" data-val="">全部问题</a><a class="type" id="typeAll" href="#" data-val="1">生活服务</a><a class="type" id="typeAll" href="#" data-val="2">问题反馈</a></p>
                 <div id="sortBox">
-                    <a href="#" class="active sortRecommended sort"><i class="material-icons dp48">thumb_up</i></a>
-                    <a href="#" class="sortTime sort"><i class="material-icons dp48">query_builder</i></a>
-                    <a href="#" class="sortHot sort"><i class="material-icons dp48">star</i></a>
+                    <a href="#" class="active sortRecommended sort" data-val="recommended"><i class="material-icons dp48">thumb_up</i></a>
+                    <a href="#" class="sortTime sort" data-val="time"><i class="material-icons dp48">query_builder</i></a>
+                    <a href="#" class="sortHot sort" data-val="hot"><i class="material-icons dp48">star</i></a>
                 </div>
             </div>
         </div>
@@ -85,7 +86,9 @@
     <script src="http://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="assets/js/materialize.js"></script>
     <script src="assets/js/timeago.min.js"></script>
+    <!--
     <script src="assets/js/dropload.min.js"></script>
+    -->
     <script src="assets/js/index.js"></script>
 </body>
 
