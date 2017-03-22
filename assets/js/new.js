0ce4fa953c1f6a222291436bfa832a73fa5125c1
liuzirui1122 <liuzirui1122@163.com>
2017-03-23 03:29:52 +0800

    new.php基本完成
    需要返回id

---
+++ assets/js/new.js
@@ -0,0 +1,84 @@
+var App = function() {
+    var app = this;
+    var errormsg = {};
+    errormsg['error'] = "提交问题时遇到数据库错误.";
+    errormsg['login first']="请先登录!";
+    errormsg['parameter error']="参数错误!";
+    
+    
+    var alertStatus = function(type) {
+        if (type=="success") return true;
+        try {
+            Materialize.toast(errormsg[type],4000);
+        } catch(e) {
+            Materialize.toast("未知错误",3000);
+        }
+        return false;
+    }
+    /*
+    var search = function(sSettings) {
+        toggleStatus("加载中...");
+        qSettings.full = false;
+        //console.log(sSettings);
+        
+    }
+*/  
+    var submitForm = function(e) {
+        if($("#quesSubject").val()=="") {
+            $("#quesSubject").focus();
+            Materialize.toast("请输入问题标题!",3000);
+            return;
+        }
+        if ($("#quesSubject").val().length>40 || $("#quesContent").val().length>500) {
+            $("#quesSubject").focus();
+            Materialize.toast("标题过长!",3000);
+            return;
+        }
+        var qdata = {};
+        qdata.type =$("input[name='quesType']:checked").val();
+        qdata.subject = $("#quesSubject").val();
+        qdata.content = $("#quesContent").val();
+        qdata.is_anonymous = Number($("#quesAnonymous").is(':checked'));
+        qdata.is_public = Number($("#quesPublic").is(':checked'));
+        $.post("API/newquestion.php",qdata,function(data){
+            console.log(data);
+            if(alertStatus(data['status'])) {
+                $("#moreBox").show();
+                try {
+                    $("#morelink").attr("href","question.php?id="+data['id']);
+                } catch(e) {
+                    console.log(e);
+                }
+            }
+        });
+    }
+    var eventInitButtons = function() {
+        $("input[name='quesType']").click(function(){
+            if ($(this).val()==2) $(".showBox").show();
+            else $(".showBox").hide();
+        });
+        $("#submitBtn").click(submitForm);
+    }
+    
+    var eventInitForms = function() {
+        $('input,textarea').characterCounter();
+    }
+    
+    var eventInit = function() {
+        //Init buttons event listeners
+        eventInitButtons();
+        //Init forms
+        eventInitForms();
+    }
+    
+    app.init = function() {
+        eventInit();
+    }
+}
+
+$(function(){
+    var app = new App();
+    app.init();  
+});
+
+
---
+++ new.php
@@ -0,0 +1,92 @@
+<?php 
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+    <title>新问题 - 北邮iHome</title>
+    <link href="assets/css/materialize.min.css" type="text/css" rel="stylesheet" />
+    <link href="assets/css/new.css" type="text/css" rel="stylesheet" />
+    <link href="assets/css/materialize_font.css" rel="stylesheet" />
+    <style>
+    </style>
+</head>
+
+<body>
+    <header>
+        <nav class="white" role="navigation">
+            <div class="nav-wrapper container hide-on-med-and-down">
+                <a href="index.php" class="brand-logo">北邮iHome</a>
+                <ul class="right">
+                  <!--
+              <?php 
+              if ($_SESSION['type'] === "admin"){
+                echo "<li><a href='admin.php'>后台管理</a></li>";
+              } 
+              ?> -->
+                    
+                    <li><a href="my.php">个人中心</a></li>
+                </ul>
+            </div>
+            <div class="nav-wrapper hide-on-large-only">
+                <a href="index.php" class="brand-logo">北邮iHome</a>
+                <ul class="left">
+                    <li><a href="my.php"><img id="user" class="circle" src="assets/img/user.png" /></a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <div class="formBox container white box">
+        <p>请选择问题类型:</p>
+        <p>
+            <input class="with-gap" name="quesType" type="radio" id="test2" value="1" checked/>
+            <label for="test2">生活服务&nbsp;</label>
+            <input class="with-gap" name="quesType" type="radio" id="test3" value="2"  />
+            <label for="test3">问题反馈&nbsp;</label>
+        </p>
+        <div class="tips">
+            <p>生活服务类问题是开放的问题，其他同学可以查看和回复;</p>
+            <p>问题反馈类问题将提交给管委会，并由管委会指派给相关组织处理，你将收到问题处理的进度通知。</p>
+        </div>
+        <br>
+        <hr>
+        <br>
+        <div class="input-field">
+            <input placeholder="标题请尽量包含问题的主要信息 " id="quesSubject" type="text" data-length="30" length="30" maxlength="30">
+            <label for="icon_prefix2">问题标题*</label>
+        </div>
+        <div class="input-field">
+            <textarea placeholder="请输入问题详情" id="quesContent" class="materialize-textarea" data-length="500" length="500" maxlength="500"></textarea>
+            <label for="textarea1">问题详情</label>
+        </div>
+        <p>
+            <input type="checkbox" class="filled-in" id="quesAnonymous" checked="checked" />
+            <label for="quesAnonymous">匿名</label>
+        </p>
+        <p class="showBox">
+            <input type="checkbox" class="filled-in" id="quesPublic"/>
+            <label for="quesPublic">公开</label>
+        </p>
+        <div class="tips showBox">
+            <p>若公开，其他同学能够在主页搜索到您的反馈，否则只有管理员和管委会可以查看。</p>
+        </div>
+        <button class="btn waves-effect waves-light" id="submitBtn">提交
+            <i class="material-icons right">send</i>
+        </button>
+        <div id="moreBox">
+            <p>问题提交成功！你可以<a href="index.php">返回主页</a>或<a id="morelink" href="#">查看该问题详情</a></p>
+        </div>
+    </div>
+    
+  
+    <script src="http://apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="assets/js/materialize.js"></script>
+    <script src="assets/js/new.js"></script>
+</body>
+
+</html>
